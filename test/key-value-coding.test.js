6d98b44c3bb79b51d7e52de16f0857cffea8a5d7
Stefan Fisk <contact@stefanfisk.com>
2014-12-09 16:58:51 +0100

    Revert "Make *ValueForKey*() support obj.*ValueForKey*() accessor pattern"
    This reverts commit 433ed4d79d082734171f52f9a9a527ef6e14865d.

--- test/key-value-coding.test.js
+++ test/key-value-coding.test.js
@@ -5,28 +5,6 @@ var getValueForKeyPath = keyValueCoding.getValueForKeyPath;
 var setValueForKeyPath = keyValueCoding.setValueForKeyPath;
 
 describe('getValueForKey()', function() {
-  it('should get using obj.getValueForKey(key)', function() {
-    var obj = {
-      getValueForKey: function() {},
-      getFoo: function() {},
-      isFoo: function() {},
-      get: function(key) {},
-      foo: 'obj[foo]'
-    };
-    var mock = sinon.mock(obj);
-
-    mock.expects('getValueForKey').once().withArgs('foo').returns('getValueForKey()');
-    mock.expects('getFoo').never().returns('getFoo()');
-    mock.expects('isFoo').never().returns('isFoo()');
-    mock.expects('get').never().withArgs('foo').returns('get()');
-
-    var value = getValueForKey(obj, 'foo');
-
-    expect(value).to.equal('getValueForKey()');
-
-    mock.verify();
-  });
-
   it('should get using obj.getKey()', function() {
     var obj = {
       getFoo: function() {},
@@ -101,25 +79,6 @@ describe('getValueForKey()', function() {
 });
 
 describe('setValueForKey()', function() {
-  it('should set using obj.setValueForKey(key)', function() {
-    var obj = {
-      setValueForKey: function(key, value) {},
-      setFoo: function() {},
-      set: function(key) {}
-    };
-    var mock = sinon.mock(obj);
-
-    mock.expects('setValueForKey').once().withArgs('foo', 'value');
-    mock.expects('setFoo').never();
-    mock.expects('set').never();
-
-    setValueForKey(obj, 'foo', 'value');
-
-    expect(obj.foo).to.equal(undefined);
-
-    mock.verify();
-  });
-
   it('should set using obj.setKey()', function() {
     var obj = {
       setFoo: function() {},
@@ -163,21 +122,6 @@ describe('setValueForKey()', function() {
 });
 
 describe('getValueForKeyPath()', function() {
-  it('should use obj.getValueForKeyPath(keyPath)', function() {
-    var obj = {
-      getValueForKeyPath: function(keyPath) {},
-      foo: 'obj[foo]'
-    };
-
-    var mock = sinon.mock(obj);
-
-    mock.expects('getValueForKeyPath').once().withArgs('foo.bar').returns('getValueForKeyPath()');
-
-    var value = getValueForKeyPath(obj, 'foo.bar');
-
-    expect(value).to.equal('getValueForKeyPath()');
-  });
-
   it('should get single', function() {
     var obj = {
       foo: 'bar'
@@ -213,20 +157,6 @@ describe('getValueForKeyPath()', function() {
 });
 
 describe('setValueForKeyPath()', function() {
-  it('should use setValueForKeyPath(keyPath, value)', function() {
-    var obj = {
-      setValueForKeyPath: function(keyPath, value) {}
-    };
-
-    var mock = sinon.mock(obj);
-
-    mock.expects('setValueForKeyPath').once().withArgs('foo.bar', 'baz').returns('getValueForKeyPath()');
-
-    setValueForKeyPath(obj, 'foo.bar', 'baz');
-
-    mock.verify();
-  });
-
   it('should set single', function() {
     var obj = {};
 
